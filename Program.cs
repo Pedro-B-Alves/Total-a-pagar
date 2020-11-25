37332e8efcd06c812b122c609e719ebd925af009
Pedro Alves <pedro22carro@gmail.com>
2020-11-24 22:37:27 -0300

    Finalizado

--- Program.cs
+++ Program.cs
@@ -11,16 +11,16 @@ namespace Total_a_pagar
             Console.WriteLine("Digite a quantidade adquirida");
             int quantidade = int.Parse(Console.ReadLine());
             Console.WriteLine("Digite o preço unitário");
-            double preco = float.Parse(Console.ReadLine());
-            double total = quantidade*preco;
+            float preco = float.Parse(Console.ReadLine());
+            float total = quantidade*preco;
             if(quantidade<=5){
-                double pagar = total - (preco*0.02);
+                float pagar = total - ((quantidade * preco) * 0.02f);
                 Console.WriteLine($"O total a pagar é {pagar} Reais.");
             }else if(quantidade>5 && quantidade<=10){
-                double pagar = total - (preco*0.03);
+                float pagar = total - ((quantidade * preco) * 0.03f);
                 Console.WriteLine($"O total a pagar é {pagar} Reais.");
             }else{
-                double pagar = total - (preco*0.05);
+                float pagar = total - ((quantidade * preco) * 0.05f);
                 Console.WriteLine($"O total a pagar é {pagar} Reais.");
             }
         }
