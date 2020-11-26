80287165039dfa067d854147d6cbbbc72cab1dad
Pedro Alves <pedro22carro@gmail.com>
2020-11-25 22:22:04 -0300

    Adicionando Brinde

--- Program.cs
+++ Program.cs
@@ -14,21 +14,31 @@ namespace Total_a_pagar
             float preco = float.Parse(Console.ReadLine());
             float total = quantidade*preco;
             Console.WriteLine($"O valor total é {total} Reais.");
-            if(quantidade<=5){
-                float desconto = ((quantidade * preco) * 0.02f);
-                Console.WriteLine($"O desconto é de {desconto} Reais.");
-                float pagar = total - desconto;
-                Console.WriteLine($"O total a pagar é {pagar} Reais.");
-            }else if(quantidade>5 && quantidade<=10){
-                float desconto = ((quantidade * preco) * 0.03f);
-                Console.WriteLine($"O desconto é de {desconto} Reais.");
-                float pagar = total - desconto;
-                Console.WriteLine($"O total a pagar é {pagar} Reais.");
+            float resultado = TotalPagamento(quantidade, preco, total);
+            if(resultado > 100){
+                Console.WriteLine("Receber brinde");
             }else{
-                float desconto = ((quantidade * preco) * 0.05f);
-                Console.WriteLine($"O desconto é de {desconto} Reais.");
-                float pagar = total - desconto;
-                Console.WriteLine($"O total a pagar é {pagar} Reais.");
+                Console.WriteLine("Receber adesivos para juntar");
+            }
+            float TotalPagamento(int quantidade, float preco, float total){
+                float pagar;
+                if(quantidade<=5){
+                    float desconto = ((quantidade * preco) * 0.02f);
+                    Console.WriteLine($"O desconto é de {desconto} Reais.");
+                    pagar = total - desconto;
+                    Console.WriteLine($"O total a pagar é {pagar} Reais.");
+                }else if(quantidade>5 && quantidade<=10){
+                    float desconto = ((quantidade * preco) * 0.03f);
+                    Console.WriteLine($"O desconto é de {desconto} Reais.");
+                    pagar = total - desconto;
+                    Console.WriteLine($"O total a pagar é {pagar} Reais.");
+                }else{
+                    float desconto = ((quantidade * preco) * 0.05f);
+                    Console.WriteLine($"O desconto é de {desconto} Reais.");
+                    pagar = total - desconto;
+                    Console.WriteLine($"O total a pagar é {pagar} Reais.");
+                }
+                return pagar;
             }
         }
     }
